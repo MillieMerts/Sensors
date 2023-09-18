1c445454426c35f5696c6298505f1fff040ea2d7
MillieMerts <119122141+MillieMerts@users.noreply.github.com>
2023-09-18 19:58:54 -0300

    edit finished

--- measurement/urls.py
+++ measurement/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from .views import CreateSensor, UpdateSensor, AddMeasurement, SensorList, SensorInfo
+from .views import GetCreateSensor, GetUpdateSensor, AddMeasurement
 
 urlpatterns = [
     # TODO: зарегистрируйте необходимые маршруты
-    path('sensor/', CreateSensor.as_view()),
-    path('sensors/<pk>/', UpdateSensor.as_view()),
+    path('sensors/', GetCreateSensor.as_view()),
+    path('sensors/<pk>/', GetUpdateSensor.as_view()),
     path('measurements/', AddMeasurement.as_view()),
-    path('sensors/', SensorList.as_view()),
-    path('sensor/info/<pk>/', SensorInfo.as_view())
 
 ]
--- measurement/views.py
+++ measurement/views.py
@@ -4,23 +4,14 @@ from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView, ListCr
 from .models import Sensor, Measurement
 from .serializers import SensorSerializer, MeasurementSerializer, SensorDetailSerializer
 
-class CreateSensor(CreateAPIView):
+class GetCreateSensor(ListCreateAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
 
-
-class UpdateSensor(RetrieveUpdateAPIView):
+class GetUpdateSensor(RetrieveUpdateAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
 
 class AddMeasurement(CreateAPIView):
     queryset = Measurement.objects.all()
     serializer_class = MeasurementSerializer
-
-class SensorList(ListCreateAPIView):
-    queryset = Sensor.objects.all()
-    serializer_class = SensorSerializer
-
-class SensorInfo(RetrieveAPIView):
-    queryset = Sensor.objects.all()
-    serializer_class = SensorDetailSerializer
